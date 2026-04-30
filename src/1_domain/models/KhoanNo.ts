eb21f7e6b85221765412cf499d34e979911e6a57
kienkhoafvp-creator <kienkhoafvp@gmail.com>
2026-04-30 20:48:00 +0700

    Xong phieu chi

--- src/1_domain/models/KhoanNo.ts
+++ src/1_domain/models/KhoanNo.ts
@@ -2,8 +2,8 @@ export interface KhoanNo {
   id?: string;
   ten_khoan_no: string;
   tong_goc_vay: number;
-  tien_tra_dinh_ky: number;
-  lai_suat_percent: number;
-  id_nguon_gan_no: string | null; // Cho phép null nếu là Nợ tiêu dùng
+  tong_tien_phai_tra: number; // Đã cập nhật theo cấu trúc mới
+  id_nguon_gan_no: string;
+  ngay_tao?: string;
   user_id?: string;
 }
